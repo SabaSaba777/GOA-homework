460f4cd50077349358395b412eca850d98ac5531
SabaSaba777 <Saba.kiba5@gmail.com>
2024-07-05 02:57:56 +0400

    commit

---
+++ level 06/homework/main.py
@@ -0,0 +1,17 @@
+#user_firstname= input("please enter your firstname: ")
+#user_lastname=input("please enter your lastname: ")
+#user_age=input("please enter your age: ")
+#user_email=input("please enter your email: ")
+
+
+
+#print("your inputed information: ",user_firstname,user_lastname,user_age,user_email )
+
+
+
+
+user_firstname= input("your firstname:")
+user_lastname= input("your lastname:")
+
+
+print("congratulations you have successfully registered:", user_firstname,user_lastname)
