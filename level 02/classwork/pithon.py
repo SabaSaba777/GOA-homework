578cc9844b3d0c4e7cf178f09f3902c5b70adb17
SabaSaba777 <Saba.kiba5@gmail.com>
2024-07-11 03:54:04 +0400

    commit

---
+++ level 02/classwork/pithon.py
@@ -0,0 +1,4 @@
+print(5+3)
+print(20-10)
+print(55 * 5)
+print(100 / 2)
---
+++ level 06/classwork/main.py
@@ -0,0 +1,9 @@
+nm1 = int(input("Please enter first number: "))
+nm2 = int(input("Please enter second number: "))
+nm3 = int(input("Please enter third number: "))
+nm4 = int(input("Please enter forth number: "))
+
+print(nm1 + nm2 + nm3 + nm4)
+print(nm1 - nm2 - nm3 - nm4)
+print(nm1 * nm2 * nm3 * nm4)
+print(nm1 / nm2 / nm3 / nm4)
