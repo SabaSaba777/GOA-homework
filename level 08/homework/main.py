72d2db7df859f3ad5b61fc708fda0276a139138f
SabaSaba777 <Saba.kiba5@gmail.com>
2024-07-17 01:28:49 +0400

    commit

---
+++ level 08/homework/main.py
@@ -0,0 +1,36 @@
+#num1=input ("please enter number:")
+#num2=input ("please enter number:")
+#num3=input ("please enter number:")
+#num4=input ("please enter number:")
+#num5=input ("please enter number:")
+
+
+
+
+#print(int(num1) + int(num1))
+#print(int(num2) - int(num2))
+#print(int(num3) * int(num3))
+#print(int(num4) / int(num4))
+#print(int(num5) % int(num5))
+
+
+
+#age= int(input("please enter your age:"))
+
+#print(age < 18 or age < 20)
+
+
+
+#print(98< 100)
+#print(20 > 11)
+#print(5 <= 10)
+#print(5 >= 1)
+#print(10 == 10)
+
+
+
+
+
+
+
+
