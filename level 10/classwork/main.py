70ab9161aa12e138c5eb0e69c3f1a744925bf30d
SabaSaba777 <Saba.kiba5@gmail.com>
2024-07-30 20:20:47 +0400

    commit

---
+++ level 10/classwork/main.py
@@ -0,0 +1,20 @@
+#password = input("Your password: ")
+#crpassword = "luka1234"
+#if password==crpassword:
+#print("თქვენ წარმატებით დარეგისტრირდით")
+#else:
+#    print("პაროლი არასწორია")
+
+
+
+
+
+product = 1
+
+for i in range(5):
+    
+    number=float(input("enter a number: " ))
+    product *=number
+
+
+    print("sum is:", product )
---
+++ level 10/homework/main.py
@@ -0,0 +1,21 @@
+num = 10
+
+for i in range(5):
+    
+    number=int(input("enter a number: " ))
+    num +=number
+
+    number=int(input("enter a number: " ))
+    num -=number
+
+    number=int(input("enter a number: " ))
+    num *=number
+
+    number=int(input("enter a number: " ))
+    num %=number
+    
+    number=int(input("enter a number: " ))
+    num /=number
+
+
+    print("answer is:", num )
